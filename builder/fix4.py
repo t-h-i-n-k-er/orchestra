36a7bd6ff14c9510f14fcde6947b7b123e2ab381
Replicant <replicant@example.com>
2026-04-25 01:20:52 -0500

    chore: remove fixing script artifacts, ignore future ones

--- builder/fix4.py
+++
@@ -1,8 +0,0 @@
-with open("agent/src/injection/module_stomp.rs", "r") as f:
-    c = f.read()
-
-c = c.replace("nt_headers.FileHeader.SizeOfOptionalHeader as usize) as *const _;", "nt_headers.FileHeader.SizeOfOptionalHeader as usize);")
-
-with open("agent/src/injection/module_stomp.rs", "w") as f:
-    f.write(c)
-
